fd4477eb52545bbac785450c8efc4ab7d9db3be3
Torres <santitorres460@gmail.com>
2025-03-31 00:16:38 -0300

    Added libs

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from '@tanstack/react-start/config';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+	server: {
+		https: true,
+	},
 	vite: {
 		plugins: [
 			tailwindcss(),
