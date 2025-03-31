507f6ddd158bfbccf9444795fecbdcb2233c540c
Torres <santitorres460@gmail.com>
2025-03-31 12:26:55 -0300

    Navbar is now inactive if the user has not given auth

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,8 +1,9 @@
-import { Link, useRouterState } from '@tanstack/react-router';
+import { Link, useParams, useRouterState } from '@tanstack/react-router';
 import { Icon } from './Icon';
 
 export const NavBar = () => {
 	const routerState = useRouterState();
+	const { membershipId } = useParams({ strict: false });
 
 	return (
 		<div className="flex gap-2 p-3 bg-gradient-to-r from-slate-900 via-slate-700 to-slate-900 fixed w-full z-50">
@@ -12,8 +13,9 @@ export const NavBar = () => {
 			</Link>
 			<div className="flex items-center mx-4 text-sm gap-6 underline-offset-6">
 				<Link
-					className={`hover:text-amber-400 transition-all duration-150 ${routerState.location.pathname.includes('/inventory') && 'underline text-amber-400'} decoration-2`}
-					to="/home"
+					className={`select-none transition-all duration-150 ${membershipId ? 'hover:text-amber-400' : 'text-white/50 pointer-events-none'} ${routerState.location.pathname.includes('/inventory') && 'underline text-amber-400'} decoration-2`}
+					to="/home/$membershipId/inventory"
+					params={{ membershipId: membershipId || '' }}
 				>
 					inventory
 				</Link>
